b09be4e43160bc7021fb0d7f79aca7a61d7c7b49
KiraTeresa <kira.soba@gmx.de>
2025-10-17 08:12:48 +0200

    use new puppy methods

--- 052-versions-example/main.go
+++ 052-versions-example/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/KiraTeresa/puppy"
+)
 
 func main() {
 	fmt.Println("Working on versions")
+
+	puppy.VersionTest()
+	puppy.VersionTest2()
 }
