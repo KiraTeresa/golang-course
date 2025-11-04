d2ddc60eef4cfc5b5ea50651b4cff7d14eb411c3
KiraTeresa <kira.soba@gmx.de>
2025-11-04 10:23:32 +0100

    120 increment int values in a map

---
+++ 015-grouping-data-values-map/120-counting-words-in-a-book/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := make(map[string]int)
+	fmt.Println(m["beautiful"])
+	m["beautiful"]++
+	fmt.Println(m["beautiful"])
+	m["beautiful"]++
+	fmt.Println(m["beautiful"])
+	m["beautiful"]++
+	fmt.Println(m["beautiful"])
+}
