80fa807ea107ebc65862cf09224bcf521046c516
KiraTeresa <kira.soba@gmx.de>
2025-11-05 10:43:26 +0100

    138 methods

---
+++ 019-functions/138-methods/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+}
+
+func (p person) speak() {
+	fmt.Println("I am", p.first)
+}
+
+func main() {
+	p1 := person{
+		first: "James",
+		last:  "Bond",
+	}
+
+	p2 := person{
+		first: "Jenny",
+		last:  "Moneypenny",
+	}
+
+	p1.speak()
+	p2.speak()
+}
