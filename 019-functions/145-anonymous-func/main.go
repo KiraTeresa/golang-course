0a0ada96e335dc141b263d4001393f61203aa958
KiraTeresa <kira.soba@gmx.de>
2025-11-07 08:10:18 +0100

    145 anonymous func

---
+++ 019-functions/145-anonymous-func/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	foo()
+
+	func() {
+		fmt.Println("Anonymous func ran")
+	}()
+
+	func(s string) {
+		fmt.Println("This is an anonymous func showing my name", s)
+	}("Kira")
+}
+
+func foo() {
+	fmt.Println("Foo ran")
+}
