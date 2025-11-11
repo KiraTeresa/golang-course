4604d48b7a80c6b32c81d3c166baccb881f06e6a
KiraTeresa <kira.soba@gmx.de>
2025-11-11 07:50:12 +0100

    173 pass by value, pointers/reference type, and mutability

---
+++ 021-pointers/173-pass-by-value-pointers-reference-type-mutability/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func intDelta(n *int) {
+	*n = 43
+}
+
+func sliceDelta(ii []int) {
+	ii[0] = 99
+}
+
+func mapDelta(md map[string]int, s string) {
+	md[s] = 43
+}
+
+func main() {
+	a := 42
+	fmt.Println(a)
+
+	intDelta(&a)   // passing the memory location of an int
+	fmt.Println(a) // passing the value
+
+	xi := []int{1, 2, 3, 4}
+	fmt.Println(xi)
+	sliceDelta(xi)
+	fmt.Println(xi)
+
+	m := make(map[string]int)
+	m["James"] = 42
+	fmt.Println(m["James"])
+	mapDelta(m, "James")
+	fmt.Println(m["James"])
+}
