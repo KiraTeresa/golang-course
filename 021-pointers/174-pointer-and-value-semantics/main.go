c1fe0baacb52ebf0d858b6c4643368485bbc9e1c
KiraTeresa <kira.soba@gmx.de>
2025-11-11 07:59:14 +0100

    174 pointer and value semantics defined

---
+++ 021-pointers/174-pointer-and-value-semantics/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+/*
+	value semantics
+
+- passing in a value to addOne and assigning it to a new variable (v)
+- v has its own memory address
+*/
+func addOne(v int) int {
+	return v + 1
+}
+
+/*
+	pointer semantics
+
+- passing in a pointer to an int
+*/
+func addOneP(v *int) {
+	*v += 1
+}
+
+func main() {
+	// value semantics
+	a := 1
+	fmt.Println(a)         // 1
+	fmt.Println(addOne(a)) // 2
+	fmt.Println(a)         // 1
+
+	fmt.Println("--------------")
+
+	// pointer semantics
+	b := 1
+	fmt.Println(b) // 1
+	addOneP(&b)
+	fmt.Println(b) // 2
+}
