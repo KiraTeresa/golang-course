3ad1bf3f1209e121928cd32e33b68f74cd4c8cd9
KiraTeresa <kira.soba@gmx.de>
2025-11-20 07:21:51 +0100

    210 hands on exercise - race condition

---
+++ 027-hands-on-exerrcises/210-hands-on-exercise-race-condition/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+/*
+● Using goroutines, create an incrementer program
+	○ have a variable to hold the incrementer value
+	○ launch a bunch of goroutines
+■ each goroutine should
+● read the incrementer value
+	○ store it in a new variable
+● yield the processor with runtime.Gosched()
+● increment the new variable
+● write the value in the new variable back to the incrementer
+variable
+● use waitgroups to wait for all of your goroutines to finish
+● the above will create a race condition.
+● Prove that it is a race condition by using the -race flag
+● if you need help, here is a hint: https://play.golang.org/p/FYGoflKQej
+*/
+
+func main() {
+	var wg sync.WaitGroup
+	incrementer := 0
+	gs := 15
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := incrementer
+			runtime.Gosched()
+			v++
+			incrementer = v
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(incrementer)
+}
