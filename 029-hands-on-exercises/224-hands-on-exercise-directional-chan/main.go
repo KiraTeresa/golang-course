799b1d06d4edc7d748647aeb2755356c17a98806
KiraTeresa <kira.soba@gmx.de>
2025-11-30 08:56:06 +0100

    224 hands on exercise directional chan

---
+++ 029-hands-on-exercises/224-hands-on-exercise-directional-chan/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+get this code running
+
+func main() {
+	cs := make(chan<- int)
+
+	go func() {
+		cs <- 42
+	}()
+	fmt.Println(<-cs)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cs\t%T\n", cs)
+}
+
+func main() {
+	cr := make(<-chan int)
+
+	go func() {
+		cr <- 42
+	}()
+	fmt.Println(<-cr)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cr\t%T\n", cr)
+}
+*/
+
+func main() {
+	ex1()
+	ex2()
+}
+
+func ex1() {
+	cs := make(chan int)
+
+	go func() {
+		cs <- 42
+	}()
+	fmt.Println(<-cs)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cs\t%T\n", cs)
+}
+
+func ex2() {
+	cr := make(chan int)
+
+	go func() {
+		cr <- 42
+	}()
+	fmt.Println(<-cr)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cr\t%T\n", cr)
+}
